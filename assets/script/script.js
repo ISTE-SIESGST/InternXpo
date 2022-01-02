6b794f4e7e70f3225552cdae00cac1fb7dcc1514
Nekros1712 <sampritchaurasiya07@gmail.com>
2022-01-02 18:47:51 +0530

    Update Date & Time - II

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("March 19, 2021 09:00:00").getTime();
+var countDownDate = new Date("January 03, 2022 09:00:00").getTime();
 var x = setInterval(function() {
   var now = new Date().getTime();
   var distance = countDownDate - now;
